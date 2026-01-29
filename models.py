8f0545de67a65ff9aa058c907a775c1641829a8b
mistergold1 <159539139+mistergold1@users.noreply.github.com>
2026-01-29 15:15:41 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+class Task:
+    def __init__(self, task_id, title, completed=False):
+        self.id = task_id
+        self.title = title
+        self.completed = completed
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "completed": self.completed
+        }
